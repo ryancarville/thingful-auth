d74d58d8e695c4a04c06149a376b51690f8fe092
Ryan Carville <ryancarville@gmail.com>
2019-06-24 15:19:34 +0200

    JWT upgrade client

--- thingful-client/src/services/thing-api-service.js
+++ thingful-client/src/services/thing-api-service.js
@@ -12,7 +12,7 @@ const ThingApiService = {
 	getThing(thingId) {
 		return fetch(`${config.API_ENDPOINT}/things/${thingId}`, {
 			headers: {
-				Authorization: `basic ${TokenService.getAuthToken()}`
+				Authorization: `bearer ${TokenService.getAuthToken()}`
 			}
 		}).then(res =>
 			!res.ok ? res.json().then(e => Promise.reject(e)) : res.json()
@@ -20,7 +20,7 @@ const ThingApiService = {
 	},
 	getThingReviews(thingId) {
 		return fetch(`${config.API_ENDPOINT}/things/${thingId}/reviews`, {
-			headers: { Authorization: `basic ${TokenService.getAuthToken()}` }
+			headers: { Authorization: `bearer ${TokenService.getAuthToken()}` }
 		}).then(res =>
 			!res.ok ? res.json().then(e => Promise.reject(e)) : res.json()
 		);
@@ -30,7 +30,7 @@ const ThingApiService = {
 			method: 'POST',
 			headers: {
 				'content-type': 'application/json',
-				Authorization: `basic ${TokenService.getAuthToken()}`
+				Authorization: `bearer ${TokenService.getAuthToken()}`
 			},
 			body: JSON.stringify({
 				thing_id: thingId,
