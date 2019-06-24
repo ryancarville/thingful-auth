bb16dde70088eb0ef3882ba67519a5711217dccf
Ryan Carville <ryancarville@gmail.com>
2019-06-24 15:17:23 +0200

    inital commit JWT

--- thingful-server/src/middleware/basic-auth.js
+++ thingful-server/src/middleware/basic-auth.js
@@ -3,15 +3,15 @@ const bcrypt = require('bcyrptjs');
 function requireAuth(req, res, next) {
 	const authToken = req.get('Authorization') || '';
 
-	let basicToken;
+	let bearerToken;
 
-	if (!authToken.toLowerCase().startsWith('basic ')) {
-		return res.status(401).json({ error: 'Missing basic token' });
+	if (!authToken.toLowerCase().startsWith('bearer ')) {
+		return res.status(401).json({ error: 'Missing bearer token' });
 	} else {
-		basicToken = authToken.slice('basic '.length, authToken.length);
+		bearerToken = authToken.slice('bearer '.length, authToken.length);
 	}
 
-	const [tokenUserName, tokenPassword] = Buffer.from(basicToken, 'base64')
+	const [tokenUserName, tokenPassword] = Buffer.from(bearerToken, 'base64')
 		.toString()
 		.split(':');
 
